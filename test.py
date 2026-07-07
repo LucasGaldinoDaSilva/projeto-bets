1eecd0b5e1e9e77f712a59ebcd080153e449bfba
Lucas Galdino da Silva <galdinolucas9@gmail.com>
2026-07-07 20:43:27 -0300

    Novas atualizações
    comecei a configuração do Airflow juntamento com o Docker onde os scripts irão ficar automatizados,
    para que rodem de forma rapida e automatica

--- test.py
+++ test.py
@@ -2,6 +2,5 @@
 
 import pandas as pd
 
-df = pd.read_parquet("data/gold/Melhor_odds/part-00000-7e9cb138-9d7f-4e4d-9db4-d98724dd356a-c000.snappy.parquet" \
-"")
+df = pd.read_parquet("data/gold/Melhor_odds/part-00000-e9e312a0-af3b-4190-be31-6ca2f9dfc066-c000.snappy.parquet")
 df
